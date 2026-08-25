902870e1a00085d5d6be0055af649d291a89ae5a
ruyi29 <983159549@qq.com>
2026-08-25 17:02:54 +0800

    chore: element-plus add

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
     <h1>What's up</h1>
+    <el-button type="primary" size="default" :icon="Plus" @click="">
+      我是一个按钮
+    </el-button>
+    <el-pagination
+      :page-sizes="[100, 200, 300, 400]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-const s = 'hello'
+//引入图标组件
+import { Plus } from '@element-plus/icons-vue'
 </script>
 
 <style scoped>
-.h1 {
+h1 {
   background-color: red;
+}
+.el-button {
+  margin-bottom: 20px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { createApp } from 'vue'
-// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//引入element-plus插件与样式（完整引入）
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-const a = 1
+//获取应用实例对象
+const app = createApp(App)
+//安装element-plus插件
+// app.use(ElementPlus)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
+//将应用挂载到挂载点上
+app.mount('#app')
