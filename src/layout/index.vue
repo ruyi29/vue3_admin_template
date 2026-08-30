9c99c832dfb5123a384368a1e7fe769be2ce2e63
ruyi29 <983159549@qq.com>
2026-08-30 15:08:59 +0800

    feat: 左侧静态菜单的搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,12 +3,30 @@
     <!-- 左侧菜单 -->
     <div class="layout_slider">
       <Logo></Logo>
+      <!-- 展示菜单 -->
+      <!-- 滚动组件 -->
+      <el-scrollbar height="400px" class="scrollbar">
+        <!-- 菜单组件 -->
+        <el-menu background-color="$base-menu-background" text-color="white">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <!-- 折叠菜单 -->
+          <el-sub-menu index="3">
+            <template #title>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部菜单 -->
     <div class="layout_tabbar">456</div>
     <!-- 内容展示区域 -->
     <div class="layout_main">
-      <p style="height: 100000px; background-color: aqua">123</p>
+      <p>123</p>
     </div>
   </div>
 </template>
@@ -21,11 +39,19 @@ import Logo from '@/layout/logo/index.vue'
 .layout_container {
   width: 100%;
   height: 100vh;
+  color: white;
+
   .layout_slider {
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
+
   .layout_tabbar {
     position: fixed;
     width: calc(100% - $base-menu-width);
@@ -34,6 +60,7 @@ import Logo from '@/layout/logo/index.vue'
     top: 0;
     left: $base-menu-width;
   }
+
   .layout_main {
     position: absolute;
     width: calc(100% - $base-menu-width);
