c88738012a3c5a01719a5073591a4feae0b46d09
ruyi29 <983159549@qq.com>
2026-08-31 00:55:09 +0800

    fix: Menu折叠图标的显示修复

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -1,51 +1,49 @@
 <template>
-  <div>
-    <template v-for="(item, index) in menuList" :key="item.path">
-      <!-- 没有子路由 -->
-      <template v-if="!item.children">
-        <el-menu-item
-          v-if="!item.meta.hidden"
-          :index="item.path"
-          @click="goRoute"
-        >
-          <template #title>
-            <el-icon>
-              <component :is="item.meta.icon"></component>
-            </el-icon>
-            <span>{{ item.meta.title }}</span>
-          </template>
-        </el-menu-item>
-      </template>
-      <!-- 有且只有一个子路由 -->
-      <template v-if="item.children && item.children.length == 1">
-        <el-menu-item
-          v-if="!item.children[0].meta.hidden"
-          :index="item.children[0].path"
-          @click="goRoute"
-        >
-          <template #title>
-            <el-icon>
-              <component :is="item.children[0].meta.icon"></component>
-            </el-icon>
-            <span>{{ item.children[0].meta.title }}</span>
-          </template>
-        </el-menu-item>
-      </template>
-      <!-- 有多个子路由 -->
-      <el-sub-menu
+  <template v-for="(item, index) in menuList" :key="item.path">
+    <!-- 没有子路由 -->
+    <template v-if="!item.children">
+      <el-menu-item
+        v-if="!item.meta.hidden"
         :index="item.path"
-        v-if="item.children && item.children.length > 1"
+        @click="goRoute"
       >
+        <el-icon>
+          <component :is="item.meta.icon"></component>
+        </el-icon>
         <template #title>
-          <el-icon>
-            <component :is="item.meta.icon"></component>
-          </el-icon>
           <span>{{ item.meta.title }}</span>
         </template>
-        <Menu :menuList="item.children"></Menu>
-      </el-sub-menu>
+      </el-menu-item>
     </template>
-  </div>
+    <!-- 有且只有一个子路由 -->
+    <template v-if="item.children && item.children.length == 1">
+      <el-menu-item
+        v-if="!item.children[0].meta.hidden"
+        :index="item.children[0].path"
+        @click="goRoute"
+      >
+        <el-icon>
+          <component :is="item.children[0].meta.icon"></component>
+        </el-icon>
+        <template #title>
+          <span>{{ item.children[0].meta.title }}</span>
+        </template>
+      </el-menu-item>
+    </template>
+    <!-- 有多个子路由 -->
+    <el-sub-menu
+      :index="item.path"
+      v-if="item.children && item.children.length > 1"
+    >
+      <template #title>
+        <el-icon>
+          <component :is="item.meta.icon"></component>
+        </el-icon>
+        <span>{{ item.meta.title }}</span>
+      </template>
+      <Menu :menuList="item.children"></Menu>
+    </el-sub-menu>
+  </template>
 </template>
 
 <script setup lang="ts">
