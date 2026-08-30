053015e678ad67125deadd93f9c1b871928fed36
ruyi29 <983159549@qq.com>
2026-08-30 14:32:25 +0800

    feat: logo组件的封装

---
+++ src/layout/logo/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logo">
+    <img :src="setting.logo" alt="logo" v-if="setting.logoHidden" />
+    <p>{{ setting.title }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+//引入设置标题与logo的配置文件
+import setting from '@/setting'
+</script>
+
+<style scoped lang="scss">
+.logo {
+  width: 100%;
+  height: $base-menu-logo-height;
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  img {
+    width: 40px;
+    height: 40px;
+  }
+  p {
+    font-size: $base-logo-title-fontSize;
+    margin-left: 10px;
+  }
+}
+</style>
