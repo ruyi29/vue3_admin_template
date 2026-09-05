3a01d93f09297a5ff9659f2e272d776fcf3b3d55
ruyi29 <983159549@qq.com>
2026-09-05 17:27:51 +0800

    feat: SPU模块数据展示

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -1,2 +1,16 @@
-// 品牌管理模块接口
+// SPU管理模块接口
 import request from '@/utils/request'
+import type { HasSpuResponseData } from './type'
+
+const API = {
+  HASSPU_URL: '/admin/product', //获取SPU列表的接口
+}
+
+export const reqHasSpu = (
+  page: number,
+  limit: number,
+  category3Id: number | string,
+) =>
+  request.get<any, HasSpuResponseData>(
+    API.HASSPU_URL + `/${page}/${limit}?category3Id=${category3Id}`,
+  )
--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -2,8 +2,14 @@
   <div>
     <Category :scene="scene" />
     <el-card style="margin: 10px 0px">
-      <el-button type="primary" icon="Plus">添加SPU</el-button>
-      <el-table border style="margin: 10px 0px">
+      <el-button
+        type="primary"
+        icon="Plus"
+        :disabled="categoryStore.c3Id ? false : true"
+      >
+        添加SPU
+      </el-button>
+      <el-table border style="margin: 10px 0px" :data="records">
         <el-table-column
           label="序号"
           type="index"
@@ -15,13 +21,39 @@
           width="120px"
           prop="spuName"
         ></el-table-column>
-        <el-table-column label="SPU描述"></el-table-column>
+        <el-table-column
+          label="SPU描述"
+          prop="description"
+          show-overflow-tooltip
+        ></el-table-column>
         <el-table-column label="SPU操作">
           <template #="{ row }">
-            <el-button type="primary" size="small" icon="Edit"></el-button>
-            <el-popconfirm title="您确定删除该SPU吗？">
+            <el-button
+              type="primary"
+              size="small"
+              icon="Plus"
+              title="添加SKU"
+            ></el-button>
+            <el-button
+              type="warning"
+              size="small"
+              icon="Edit"
+              title="修改SPU"
+            ></el-button>
+            <el-button
+              type="info"
+              size="small"
+              icon="View"
+              title="查看SKU列表"
+            ></el-button>
+            <el-popconfirm :title="`您确定删除“${row.spuName}”吗？`">
               <template #reference>
-                <el-button type="danger" size="small" icon="Delete"></el-button>
+                <el-button
+                  type="danger"
+                  size="small"
+                  icon="Delete"
+                  title="删除SPU"
+                ></el-button>
               </template>
             </el-popconfirm>
           </template>
@@ -33,18 +65,51 @@
         :page-sizes="[3, 5, 7, 9]"
         :background="true"
         layout="prev, pager, next, jumper, ->, sizes, total"
-        :total="400"
+        :total="total"
+        @current-change="getHasSpu"
+        @size-change="changeSize"
       />
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import useCatoryStore from '@/store/modules/category'
+import { reqHasSpu } from '@/api/product/spu'
+import type { HasSpuResponseData, Records } from '@/api/product/spu/type'
 
+let categoryStore = useCatoryStore()
 let scene = ref<number>(0) //card组件内容切换变量；0：table，1：添加与修改
 let pageNo = ref<number>(1)
 let pageSize = ref<number>(3)
+let records = ref<Records>([])
+let total = ref<number>(0)
+
+watch(
+  () => categoryStore.c3Id,
+  () => {
+    if (!categoryStore.c3Id) return
+    getHasSpu()
+  },
+)
+
+const getHasSpu = async (pager = 1) => {
+  pageNo.value = pager
+  let res: HasSpuResponseData = await reqHasSpu(
+    pageNo.value,
+    pageSize.value,
+    categoryStore.c3Id,
+  )
+  if (res.code == 200) {
+    records.value = res.data.records
+    total.value = res.data.total
+  }
+}
+
+const changeSize = () => {
+  getHasSpu()
+}
 </script>
 
 <style scoped></style>
