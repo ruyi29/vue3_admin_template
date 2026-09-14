88e900c28f493897d4540149f845997c3ab9e459
ruyi29 <983159549@qq.com>
2026-09-14 23:39:16 +0800

    feat: 用户管理页面静态搭建

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>
-    <p>user</p>
-  </div>
+  <el-card style="height: 80px">
+    <el-form :inline="true" class="form">
+      <el-form-item label="用户名：">
+        <el-input placeholder="请输入搜索用户名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">搜索</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <el-card style="margin: 10px 0">
+    <el-button type="primary">添加用户</el-button>
+    <el-button type="danger">批量删除</el-button>
+    <el-table style="margin: 10px 0" border>
+      <el-table-column type="selection"></el-table-column>
+      <el-table-column label="#" type="index" align="center"></el-table-column>
+      <el-table-column label="ID" align="center"></el-table-column>
+      <el-table-column label="用户名字" align="center"></el-table-column>
+      <el-table-column label="用户名称" align="center"></el-table-column>
+      <el-table-column label="用户角色" align="center"></el-table-column>
+      <el-table-column label="创建时间" align="center"></el-table-column>
+      <el-table-column label="更新时间" align="center"></el-table-column>
+      <el-table-column label="操作" align="center" width="260px">
+        <template #>
+          <el-button type="primary" icon="">分配角色</el-button>
+          <el-button type="primary" icon="Edit">编辑</el-button>
+          <el-button type="primary" icon="Delete">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      v-model:current-page="pageNo"
+      v-model:page-size="pageSize"
+      :page-sizes="[5, 7, 9, 11]"
+      :background="true"
+      layout="prev, pager, next, jumper, ->, sizes, total"
+      :total="100"
+    ></el-pagination>
+  </el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<style scoped></style>
+let pageNo = ref<number>(1)
+let pageSize = ref<number>(5)
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
