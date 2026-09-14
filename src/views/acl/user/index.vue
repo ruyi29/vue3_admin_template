56d7df509f42d1204206a78895fb8c5ed11891b7
ruyi29 <983159549@qq.com>
2026-09-15 00:33:38 +0800

    feat: 修改与添加用户的静态搭建

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -11,7 +11,7 @@
     </el-form>
   </el-card>
   <el-card style="margin: 10px 0">
-    <el-button type="primary">添加用户</el-button>
+    <el-button type="primary" @click="addUser">添加用户</el-button>
     <el-button type="danger">批量删除</el-button>
     <el-table style="margin: 10px 0" border :data="userArr">
       <el-table-column type="selection"></el-table-column>
@@ -57,7 +57,14 @@
           <el-button type="primary" icon="User" size="small">
             分配角色
           </el-button>
-          <el-button type="primary" icon="Edit" size="small">编辑</el-button>
+          <el-button
+            type="primary"
+            icon="Edit"
+            size="small"
+            @click="updateUser(row)"
+          >
+            编辑
+          </el-button>
           <el-button type="primary" icon="Delete" size="small">删除</el-button>
         </template>
       </el-table-column>
@@ -73,18 +80,44 @@
       @size-change="handler"
     ></el-pagination>
   </el-card>
+  <!-- 抽屉 添加/更新用户 -->
+  <el-drawer v-model="drawer">
+    <template #header>
+      <h4>添加用户</h4>
+    </template>
+    <template #default>
+      <el-form>
+        <el-form-item label="用户姓名">
+          <el-input placeholder="请您输入用户姓名"></el-input>
+        </el-form-item>
+        <el-form-item label="用户名称">
+          <el-input placeholder="请您输入用户名称"></el-input>
+        </el-form-item>
+        <el-form-item label="用户密码">
+          <el-input placeholder="请您输入用户密码"></el-input>
+        </el-form-item>
+      </el-form>
+    </template>
+    <template #footer>
+      <div style="flex: auto">
+        <el-button type="primary">确定</el-button>
+        <el-button>取消</el-button>
+      </div>
+    </template>
+  </el-drawer>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { reqUserInfo } from '@/api/acl/user/index'
-import type { UserResponseData, Records } from '@/api/acl/user/type'
+import type { UserResponseData, Records, User } from '@/api/acl/user/type'
 import { ElMessage } from 'element-plus'
 
 let pageNo = ref<number>(1)
 let pageSize = ref<number>(5)
 let total = ref<number>(0)
 let userArr = ref<Records>([])
+let drawer = ref<boolean>(false) //控制添加、更新用户的抽屉
 
 onMounted(() => {
   getHasUser()
@@ -101,6 +134,14 @@ const getHasUser = async (pager = 1) => {
 }
 const handler = () => {
   getHasUser()
+}
+//添加用户
+const addUser = () => {
+  drawer.value = true
+}
+//更新用户
+const updateUser = (row: User) => {
+  drawer.value = true
 }
 </script>
 
