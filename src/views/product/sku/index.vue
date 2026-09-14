9645929b9d8f6e6ec13a6a87a66a7bdf202cd9be
ruyi29 <983159549@qq.com>
2026-09-14 17:09:50 +0800

    feat: SKU模块静态搭建

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>
-    <p>sku</p>
-  </div>
+  <el-card>
+    <el-table border style="margin: 10px 0">
+      <el-table-column
+        label="序号"
+        type="index"
+        align="center"
+        width="80px"
+      ></el-table-column>
+      <el-table-column
+        label="名称"
+        show-overflow-tooltip
+        width="150px"
+      ></el-table-column>
+      <el-table-column
+        label="描述"
+        show-overflow-tooltip
+        width="150px"
+      ></el-table-column>
+      <el-table-column label="默认图片" width="150px"></el-table-column>
+      <el-table-column label="重量" width="150px"></el-table-column>
+      <el-table-column label="价格" width="150px"></el-table-column>
+      <el-table-column
+        label="操作"
+        width="250px"
+        fixed="right"
+      ></el-table-column>
+    </el-table>
+    <el-pagination
+      v-model:current-page="pageNo"
+      v-model:page-size="pageSize"
+      :page-sizes="[10, 20, 30, 40]"
+      :background="true"
+      layout="prev, pager, next, jumper, ->, sizes, total"
+      :total="400"
+    ></el-pagination>
+  </el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let pageNo = ref<number>(1)
+let pageSize = ref<number>(10)
+</script>
 
 <style scoped></style>
